f869b6fff388f4fe5c4d2015a354a4429e2706f7
Rohit kumar <rohit.rksaini28@gmail.com>
2026-08-09 12:40:21 +0530

    list

---
+++ List/list.py
@@ -0,0 +1,8 @@
+list = [12,15,"rohit","abc"]
+print(list[2][0])
+
+l=[1,2,3,4]
+for n in l:
+    l[-1]=n
+
+print(l)
---
+++ taskList&String.py/task1.py
@@ -0,0 +1,11 @@
+# aadhar card number vaildition(12 digit) in number
+
+userInput = input("Enter the aadhar number ")
+
+# if userInput.isdigit() and len(userInput) == 12:
+#     print("aadhar number is vaild")
+# else:
+#     print("aadhar number is not vaild")
+
+if userInput.isdigit("False"):
+    print("a")
